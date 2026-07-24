628811804dec21cd767eaaff420609f2bf5826b5
moazzam-007 <218071881+moazzam-007@users.noreply.github.com>
2026-07-24 23:25:44 +0530

    fix compile error: import android.view.View in MainActivity

--- app/src/main/java/com/wifioptimizer/MainActivity.java
+++ app/src/main/java/com/wifioptimizer/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.VpnService;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
